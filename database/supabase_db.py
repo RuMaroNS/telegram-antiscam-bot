92b27752e4e4fad680968a9f6ea9de2ae36588d7
RuMaroNS <sdulimos@gmail.com>
2026-06-05 02:08:16 +0700

    Update Supabase key environment variable

--- database/supabase_db.py
+++ database/supabase_db.py
@@ -3,7 +3,7 @@ from supabase import create_client, Client
 
 # Инициализация клиента Supabase
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
 
 if not SUPABASE_URL or not SUPABASE_KEY:
     raise ValueError("Переменные окружения SUPABASE_URL или SUPABASE_KEY не заданы!")
